a80871d86ef812560dc12efc32ee74f9ccb6883e
Othmane N <59323287+iietmoon@users.noreply.github.com>
2020-06-26 12:43:47 +0100

    onDev

--- index.php
+++ index.php
@@ -17,32 +17,45 @@
   </head>
   <body>
      <!-- Navbar Section -->
-     <section>
+     <section class="ns-bg pb-1">
         <!-- Ship + Discount -->
         <nav class="ns-bg main-nav-pd pt-2" dir="ltr">
           <ul class="list-inline">
-                <li class="list-inline-item"><a class="ns-text-a" href="#">أفضل عروض<i class="fa fa-percent ml-2 ns-text-i" aria-hidden="true"></i></a></li>
-                <li class="list-inline-item"><a class="ns-text-a" href="#">إرجاع مجاني بسهولة<i class="fa fa-refresh ml-2 ns-text-i" aria-hidden="true"></i></a></li>
+                <li class="list-inline-item"><a class="ns-text-a" href="#">أفضل عروض <img src="helpers/bootstrap-icons/lightning.svg" alt="" width="18" height="18" title="Bootstrap"></a></li>
+                <li class="list-inline-item"><a class="ns-text-a" href="#">إرجاع مجاني بسهولة <img src="helpers/bootstrap-icons/arrow-repeat.svg" alt="" width="18" height="18" title="Bootstrap"></a></li>
           </ul>
         </nav>
         <!-- Navigation -->
         <nav class="ns-bg main-nav-pd" dir="ltr">
           <ul class="list-inline ">
-                <li class="list-inline-item pr-2"><a class="nm-text-a" href="#"><i class="fa fa-shopping-bag mr-2 nm-text-i" aria-hidden="true"></i>عربة التسوق</a></li>
+                <li class="list-inline-item pr-2"><a class="nm-text-a" href="#"><img src="helpers/bootstrap-icons/bag.svg" alt="" width="20" height="20" title="Bootstrap" class="pr-1">عربة التسوق</a></li>
                 <li class="list-inline-item left-divider pl-2">
                   <div class="dropdown mr-1">
                     <button type="button" class="btn .nm-text-a" id="dropdownMenuOffset" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-offset="10,20">
                       <a class="nm-text-a" href="#"><i class="fa fa-caret-down mr-2 ml-2 ns-text-i" aria-hidden="true"></i>تسجيل الدخول أو الإشتراك</a>  
                     </button>
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenuOffset">
-                         <a class="dropdown-item" href="#">Action</a>
-                         <a class="dropdown-item" href="#">Another action</a>
-                         <a class="dropdown-item" href="#">Something else here</a>
+                    <div class="dropdown-menu nm-drop border-0 text-center p-3" aria-labelledby="dropdownMenuOffset">
+                         <p class="nm-drop-text-a" >عملاء تاجر السابقين</p>
+                         <button type="button" class="btn btn-primary font text-center rounded-0 pl-4 pr-4 pb-2">تسجيل الدخول</button>
+                         <div class="dropdown-divider mt-3 mb-3"></div>
+                         <p class="nm-drop-text-a" >ليس لديك حساب؟</p>
+                         <a class="text-center text-primary nm-drop-text-a" href="">اشترك الآن</a> 
                     </div>
                   </div>
                 </li>
-                
-                
+                <li class="list-inline-item pl-3 nm-form"> 
+                    <div class="input-group mb-3 nm-form-bg pt-1 pb-1 pr-2 pl-2">
+                         <div class="input-group-prepend">
+                              <button class="btn btn-outline-secondary rounded-0 border-0" type="button">
+                                <img src="helpers/bootstrap-icons/search.svg" alt="" width="18" height="18" title="Bootstrap">
+                              </button>
+                         </div>
+                         <input dir="rtl"  type="text" class="form-control rounded-0 border-0 nm-form font" placeholder="ما الذي تبحث عنه؟" aria-label="Username" aria-describedby="basic-addon1">
+                    </div>
+                </li>
+                <li class="list-inline-item pl-3"> 
+                   <img src="upload/main/logo.png" alt="tajer logo" width="100" />
+                </li>
           </ul>
         </nav>
      </section>
