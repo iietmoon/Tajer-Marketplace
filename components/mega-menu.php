b15e5d2f823e56d25a85a080c8aeaa56c41bcd67
Othmane N <59323287+iietmoon@users.noreply.github.com>
2020-06-28 00:55:51 +0100

    onDev

---
+++ components/mega-menu.php
@@ -0,0 +1,102 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-white shadow-sm" id="mega-menu" dir="rtl">
+  <div class="collapse navbar-collapse " id="main_nav">
+      <ul class="navbar-nav pb-2 pt-2">
+	     <li class="nav-item dropdown has-megamenu pl-5 border-left">
+	        <a class="mgm-text" href="#" data-toggle="dropdown"> جميع الفئات <img src="helpers/bootstrap-icons/caret-down-fill.svg" alt="" width="30" height="30" title="Bootstrap" class="pr-3"></a>
+	          <div class="dropdown-menu megamenu">
+		    
+              This is content of megamenu. <br>
+	          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	         	consequat.
+		        
+              </div> <!-- dropdown-mega-menu.// -->
+         </li>
+         <li class="nav-item dropdown has-megamenu pr-4 ">
+	        <a class="mgm-text" href="#" data-toggle="dropdown"> الإلكترونيات </a>
+	          <div class="dropdown-menu megamenu">
+		    
+              This is content of megamenu. <br>
+	          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	         	consequat.
+		        
+              </div> <!-- dropdown-mega-menu.// -->
+         </li>
+         <li class="nav-item dropdown has-megamenu pr-4 ">
+	        <a class="mgm-text" href="#" data-toggle="dropdown"> الأزياء </a>
+	          <div class="dropdown-menu megamenu">
+		    
+              This is content of megamenu. <br>
+	          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	         	consequat.
+		        
+              </div> <!-- dropdown-mega-menu.// -->
+         </li>
+         <li class="nav-item dropdown has-megamenu pr-4 ">
+	        <a class="mgm-text" href="#" data-toggle="dropdown"> المنزل </a>
+	          <div class="dropdown-menu megamenu">
+		    
+              This is content of megamenu. <br>
+	          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	         	consequat.
+		        
+              </div> <!-- dropdown-mega-menu.// -->
+         </li>
+         <li class="nav-item dropdown has-megamenu pr-4 ">
+	        <a class="mgm-text" href="#" data-toggle="dropdown"> الجمال والعطور </a>
+	          <div class="dropdown-menu megamenu">
+		    
+              This is content of megamenu. <br>
+	          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	         	consequat.
+		        
+              </div> <!-- dropdown-mega-menu.// -->
+         </li>
+         <li class="nav-item dropdown has-megamenu pr-4 ">
+	        <a class="mgm-text" href="#" data-toggle="dropdown"> المواليد و الألعاب </a>
+	          <div class="dropdown-menu megamenu">
+		    
+              This is content of megamenu. <br>
+	          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	         	consequat.
+		        
+              </div> <!-- dropdown-mega-menu.// -->
+         </li>
+         <li class="nav-item dropdown has-megamenu pr-4 ">
+	        <a class="mgm-text" href="#" data-toggle="dropdown"> البقالة </a>
+	          <div class="dropdown-menu megamenu">
+		    
+              This is content of megamenu. <br>
+	          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	         	consequat.
+		        
+              </div> <!-- dropdown-mega-menu.// -->
+         </li>
+         <li class="nav-item dropdown has-megamenu pr-4 ">
+	        <a class="mgm-text" href="#" data-toggle="dropdown"> الرياضة </a>
+	          <div class="dropdown-menu megamenu">
+		    
+              This is content of megamenu. <br>
+	          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	         	consequat.
+		        
+              </div> <!-- dropdown-mega-menu.// -->
+         </li>
+       </ul>
+    </div> <!-- navbar-collapse.// -->
+</nav>
