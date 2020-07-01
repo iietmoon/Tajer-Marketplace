7e773d43f88f0a2f5b5c9bd935c6ad45e7bd8b67
Othmane N <59323287+iietmoon@users.noreply.github.com>
2020-07-01 16:03:08 +0100

    onDev

--- components/product-wp.php
+++ components/product-wp.php
@@ -11,22 +11,95 @@
         <h1 class="product-wp-title">عروض أسبوعية <span id="counter-down" class="product-wp-counter">  </span> </h1>
     </div>
     <!-- Products -->
-    <div class="container justify-content-center mt-5">
+    <div class=" mt-4 mb-5">
          <div class="row">
-              <div class="col product-wp-p">
-                  <div>
-                      <img src="https://s.alicdn.com/@sc01/kf/Hbb55f87720cb4669ab8d062f1391284bV.jpg_220x220.jpg" width="80%" alt="" class="">
+              <!-- Product item -->
+              <div class="col-md product-wp-p ml-3 mr-3" href="#">
+                <a href="#">
+                  <div class="product-wp-p-img text-center">
+                      <img src="https://s.alicdn.com/@sc01/kf/Hbb55f87720cb4669ab8d062f1391284bV.jpg_220x220.jpg"  alt="" class="">
                   </div>
-                  <div class="text-right pt-3">
+                  <div class="text-right pt-2">
                       <h1 class="product-wp-p-title">ترينديول تيشيرت بطبعة على الصدر أسود</h1>
                       <p class="product-wp-p-price">636.00 جنيه <span class="product-wp-p-discount">60% خصم</span></p>
                       <p></p>
                   </div>
+                </a>
               </div>
-              <div class="col">col</div>
-              <div class="col">col</div>
-              <div class="col">col</div>
-              <div class="col">col</div>
-  </div>
-
+              <!-- End of Product item -->    
+              <!-- Product item -->
+              <div class="col-md product-wp-p ml-3 mr-3" href="#">
+                <a href="#">
+                  <div class="product-wp-p-img text-center">
+                      <img src="https://cf2.s3.souqcdn.com/item/2016/12/20/11/78/21/20/item_XL_11782120_18090703.jpg"  alt="" class="">
+                  </div>
+                  <div class="text-right pt-2">
+                      <h1 class="product-wp-p-title">انفنيكس زيرو 4 X555 بشريحتي اتصال - 32 جيجا ، 3 جيجا رام ، الجيل الرابع ال تي اي ، رمادي</h1>
+                      <p class="product-wp-p-price">2,599.00 جنيه <span class="product-wp-p-discount">60% خصم</span></p>
+                      <p></p>
+                  </div>
+                </a>
+              </div>
+              <!-- End of Product item -->    
+              <!-- Product item -->
+              <div class="col-md product-wp-p ml-3 mr-3" href="#">
+                <a href="#">
+                  <div class="product-wp-p-img text-center">
+                      <img src="https://cf4.s3.souqcdn.com/item/2018/02/12/30/64/53/34/item_XL_30645334_110387160.jpg"  alt="" class="">
+                  </div>
+                  <div class="text-right pt-2">
+                      <h1 class="product-wp-p-title">White Point Wpw 7815 Ps غسالة تحميل امامي ، 7 كيلو - فضي</h1>
+                      <p class="product-wp-p-price">4,499.00 جنيه <span class="product-wp-p-discount">60% خصم</span></p>
+                      <p></p>
+                  </div>
+                </a>
+              </div>
+              <!-- End of Product item -->
+              <!-- Product item -->
+              <div class="col-md product-wp-p ml-3 mr-3" href="#">
+                <a href="#">
+                  <div class="product-wp-p-img text-center">
+                      <img src="https://cf5.s3.souqcdn.com/item/2016/05/08/10/70/00/83/item_XL_10700083_14220481.jpg"  alt="" class="">
+                  </div>
+                  <div class="text-right pt-2">
+                      <h1 class="product-wp-p-title">ترينديول تيشيرت بطبعة على الصدر أسود</h1>
+                      <p class="product-wp-p-price">636.00 جنيه <span class="product-wp-p-discount">60% خصم</span></p>
+                      <p></p>
+                  </div>
+                </a>
+              </div>
+              <!-- End of Product item -->    
+              <!-- Product item -->
+              <div class="col-md product-wp-p ml-3 mr-3" href="#">
+                <a href="#">
+                  <div class="product-wp-p-img text-center">
+                      <img src="https://cf2.s3.souqcdn.com/item/2016/12/20/11/78/21/20/item_XL_11782120_18090703.jpg"  alt="" class="">
+                  </div>
+                  <div class="text-right pt-2">
+                      <h1 class="product-wp-p-title">انفنيكس زيرو 4 X555 بشريحتي اتصال - 32 جيجا ، 3 جيجا رام ، الجيل الرابع ال تي اي ، رمادي</h1>
+                      <p class="product-wp-p-price">2,599.00 جنيه <span class="product-wp-p-discount">60% خصم</span></p>
+                      <p></p>
+                  </div>
+                </a>
+              </div>
+              <!-- End of Product item -->    
+              <!-- Product item -->
+              <div class="col-md product-wp-p ml-3 mr-3" href="#">
+                <a href="#">
+                  <div class="product-wp-p-img text-center">
+                      <img src="https://cf4.s3.souqcdn.com/item/2018/02/12/30/64/53/34/item_XL_30645334_110387160.jpg"  alt="" class="">
+                  </div>
+                  <div class="text-right pt-2">
+                      <h1 class="product-wp-p-title">White Point Wpw 7815 Ps غسالة تحميل امامي ، 7 كيلو - فضي</h1>
+                      <p class="product-wp-p-price">4,499.00 جنيه <span class="product-wp-p-discount">60% خصم</span></p>
+                      <p></p>
+                  </div>
+                </a>
+              </div>
+              <!-- End of Product item -->    
+              
+               
+              
+              
+    </div>
 </section>
