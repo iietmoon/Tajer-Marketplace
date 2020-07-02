5a05413aae774ae76dca05ea96432a5ab1932f48
Othmane N <59323287+iietmoon@users.noreply.github.com>
2020-07-02 11:28:46 +0100

    onDev

--- components/product-wp.php
+++ components/product-wp.php
@@ -16,7 +16,7 @@
               <!-- Product item -->
               <div class="col-md product-wp-p ml-3 mr-3" href="#">
                 <a href="#">
-                  <div class="product-wp-p-img text-center">
+                  <div class="product-wp-p-img text-center d-flex align-items-center">
                       <img src="https://s.alicdn.com/@sc01/kf/Hbb55f87720cb4669ab8d062f1391284bV.jpg_220x220.jpg"  alt="" class="">
                   </div>
                   <div class="text-right pt-2">
@@ -30,7 +30,7 @@
               <!-- Product item -->
               <div class="col-md product-wp-p ml-3 mr-3" href="#">
                 <a href="#">
-                  <div class="product-wp-p-img text-center">
+                  <div class="product-wp-p-img text-center d-flex align-items-center">
                       <img src="https://cf2.s3.souqcdn.com/item/2016/12/20/11/78/21/20/item_XL_11782120_18090703.jpg"  alt="" class="">
                   </div>
                   <div class="text-right pt-2">
@@ -44,7 +44,7 @@
               <!-- Product item -->
               <div class="col-md product-wp-p ml-3 mr-3" href="#">
                 <a href="#">
-                  <div class="product-wp-p-img text-center">
+                  <div class="product-wp-p-img text-center d-flex align-items-center">
                       <img src="https://cf4.s3.souqcdn.com/item/2018/02/12/30/64/53/34/item_XL_30645334_110387160.jpg"  alt="" class="">
                   </div>
                   <div class="text-right pt-2">
@@ -58,7 +58,7 @@
               <!-- Product item -->
               <div class="col-md product-wp-p ml-3 mr-3 " href="#">
                 <a href="#">
-                  <div class="product-wp-p-img text-center">
+                  <div class="product-wp-p-img text-center d-flex align-items-center">
                       <img src="https://cf2.s3.souqcdn.com/item/2016/01/04/98/03/36/6/item_XL_9803366_11540464.jpg"  alt="" class="">
                   </div>
                   <div class="text-right pt-2">
@@ -72,7 +72,7 @@
               <!-- Product item -->
               <div class="col-md product-wp-p ml-3 mr-3" href="#">
                 <a href="#">
-                  <div class="product-wp-p-img text-center">
+                  <div class="product-wp-p-img text-center d-flex align-items-center">
                       <img src="https://cf2.s3.souqcdn.com/item/2019/03/06/46/49/80/75/item_XXL_46498075_4bb160f2d8e8f.jpg"  alt="" class="">
                   </div>
                   <div class="text-right pt-2">
@@ -86,7 +86,7 @@
               <!-- Product item -->
               <div class="col-md product-wp-p ml-3 mr-3" href="#">
                 <a href="#">
-                  <div class="product-wp-p-img text-center">
+                  <div class="product-wp-p-img text-center d-flex align-items-center">
                       <img src="https://cf4.s3.souqcdn.com/item/2018/02/12/30/64/53/34/item_XL_30645334_110387160.jpg"  alt="" class="">
                   </div>
                   <div class="text-right pt-2">
